8f991210e5b33346d618fd81eceebc8fe2a778d4
Raviteja1652 <ravitejamrt.165@gmail.com>
2023-11-22 16:52:17 +0530

    adding click event and changing value using useState

--- src/Components/Expenses/ExpenseDetails.js
+++ src/Components/Expenses/ExpenseDetails.js
@@ -1,12 +1,20 @@
 import './ExpenseItem.css'
-
+import { useState } from 'react';
 
 const ExpenseDetails = (props) => {
+    const [expenseAmount, setexpenseAmount] = useState(props.expenseAmount)
+
+    const clickHandler = () => {
+        setexpenseAmount('100')
+        console.log('Clicked')
+    }
+
     return (
     <div className='expense-item__description'>
         <h2>{ props.expenseTitle }</h2>
         <div>LocationOfExpenditure: { props.expenseLocation }</div>
-        <div className='expense-item__price'>₹{ props.expenseAmount }</div>
+        <div className='expense-item__price'>₹{ expenseAmount }</div>
+        <button onClick={ clickHandler }>Change Title</button>
     </div>
     )
 }
