3aae0ade1b97f31bdef5c3f88f6aaf39ece704de
Raviteja1652 <ravitejamrt.165@gmail.com>
2023-11-24 01:24:59 +0530

    able to shoe entered expense data on the UI and added unique key

--- src/App.js
+++ src/App.js
@@ -4,43 +4,19 @@ import Card from "./Components/UI/Card";
 import NewExpense from "./Components/NewExpense/NewExpense";
 import ExpenseFilter from "./Components/Expenses/ExpenseFilter";
 import { useState } from "react";
+const Dummy_Data = [
+  { 
+    id: 'e2', 
+    amount: 799.49,
+    title: 'New TV',  
+    date: new Date(2021, 2, 12),
+  },
+];
 
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      location: 'Chennai'
-    },
-    { 
-      id: 'e2', 
-      title: 'New TV', 
-      amount: 799.49, 
-      date: new Date(2021, 2, 12), 
-      location: 'Banglore'
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      location: 'Hyderabad'
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      location: 'Pune'
-    },
-  ];
-
+  const [expenses, setExpenses] = useState(Dummy_Data)
   const addExpenseHandler = (expense) => {
-    console.log('in App.js!')
-    console.log(expense)
-    
+    setExpenses((prevExpenses) => { return [expense, ...prevExpenses]})
   }
   const [filteredYear, setFilteredYear] = useState('2020');
   const filterChangeHandler = (selectedYear) => {
@@ -52,30 +28,14 @@ function App() {
       <NewExpense onSubmittingExpense={addExpenseHandler} />
       <Card className="expenses">
         <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-        <ExpenseItem 
-          expenseDate = { expenses[0].date }
-          expenseTitle = { expenses[0].title }
-          expenseAmount = { expenses[0].amount }
-          expenseLocation = { expenses[0].location }
-        />
-        <ExpenseItem 
-          expenseDate = { expenses[1].date }
-          expenseTitle = { expenses[1].title }
-          expenseAmount = { expenses[1].amount }
-          expenseLocation = { expenses[1].location }
-        ></ExpenseItem>
-        <ExpenseItem 
-          expenseDate = { expenses[2].date }
-          expenseTitle = { expenses[2].title }
-          expenseAmount = { expenses[2].amount }
-          expenseLocation = { expenses[2].location }
-        ></ExpenseItem>
-        <ExpenseItem 
-          expenseDate = { expenses[3].date }
-          expenseTitle = { expenses[3].title }
-          expenseAmount = { expenses[3].amount }
-          expenseLocation = { expenses[3].location }
-        ></ExpenseItem>
+        {expenses.map((expense) => (
+          <ExpenseItem 
+            key={expense.id}
+            expenseTitle={expense.title}
+            expenseAmount={expense.amount}
+            expenseDate={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
