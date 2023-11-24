0eee92b83ee9e4106a560806b993da174d52b781
Raviteja1652 <ravitejamrt.165@gmail.com>
2023-11-24 14:25:55 +0530

    Conditional rendering

---
+++ src/Components/Expenses/ExpensesList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import './ExpensesList.css'
+
+const ExpensesList = (props) => {
+    let listOfExpenses = props.items.map((expense) => (
+        <ExpenseItem 
+          key={expense.id}
+          expenseTitle={expense.title}
+          expenseAmount={expense.amount}
+          expenseDate={expense.date}
+        />
+    ))
+
+    if (props.items.length <= 1){
+        if(props.items.length === 1){
+            return (
+                <>
+                {listOfExpenses}
+                <h3 className="expenses-list__fallback_single"> Only single Expense here. Please add more... </h3>
+                </>
+            )
+        }
+        return <h3 className="expenses-list__fallback"> -- No Expenses found! -- </h3>
+    }
+
+    return (
+        <ul className="expenses-list">
+            {listOfExpenses}
+        </ul>
+    )
+}
+
+export default ExpensesList
